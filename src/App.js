57c597417d0b5658a641e1dfdfd8a7c52f28f954
Augusto Balansó <38744332+augustobalanso@users.noreply.github.com>
2022-05-15 04:58:50 -0300

    Segundo desafio

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import ItemListContainer from './components/itemListContainer/itemListContainer.js'
 import NavBar from './components/NavBar/navbar.js';
-import { Container } from '@mui/material';
+import { Container, Grid } from '@mui/material';
 
 
 
@@ -10,7 +10,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Container>
-          <ItemListContainer />
+        <Grid container>
+          <ItemListContainer title={"Undercover Dunk Hi 1985 Sneakers Black"} price={53000} image={"./img/wookies-prod1.jpg"} />
+          <ItemListContainer title={"Air Force 1 '07 QS 'UNO' Sneakers Multicolor"} price={65000} image={"./img/wookies-prod2.jpg"} />
+          <ItemListContainer title={"Forum 84 Hi AEC Sneakers Blue"} price={45000} image={"./img/wookies-prod4.jpg"} />
+        </Grid>
       </Container>
     </div>
   );
--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -3,30 +3,25 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActions, Grid } from '@mui/material';
-import arrayProds from '../Productos/backendsimulado.js';
+// import arrayProds from '../Productos/backendsimulado.js';
 
-const ItemListContainer = () => {
+const ItemListContainer = ({ image, title, price}) => {
 
     return (
-        <Grid container direction="row" alignItems="center" justify="center" spacing={3}>
-            {arrayProds.map((productos) =>
-                <Grid item xs={12} md={6} lg={4} sx={{display : 'flex', flexDirection : 'column', alignItems : 'center'}} key={productos.id}>
+            <Grid item md={4}>
                 <Card sx={{ maxWidth: 345, marginTop: '50px' }} variant={'outlined'}>
                     <CardMedia
                         component="img"
                         height="100%"
-                        image={productos.img}
-                        alt="green iguana"
+                        image={image}
+                        alt={title}
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
-                            {productos.title}
-                        </Typography>
-                        <Typography variant="body2" color="text.secondary">
-                            {productos.itemDesc}
+                            {title}
                         </Typography>
                         <Typography variant="h6" color="text.primary">
-                            ${productos.price}
+                            ${price}
                         </Typography>
                     </CardContent>
                     <CardActions>
@@ -36,9 +31,41 @@ const ItemListContainer = () => {
                         </div>
                     </CardActions>
                 </Card>
-                </Grid>
-            )}
-        </Grid>
+            </Grid>
+
+        // ESTO ES PARA CUANDO SE MAPEE LA DATA, COMO EN ESTE DESAFIO SE PIDE ESPECIFICAMENTE PROPS, LO TUVE QUE SACAR
+        // 
+        // <Grid container direction="row" alignItems="center" justify="center" spacing={3}>
+        //     {arrayProds.map((productos) =>
+        //         <Grid item xs={12} md={6} lg={4} sx={{display : 'flex', flexDirection : 'column', alignItems : 'center'}} key={productos.id}>
+        //         <Card sx={{ maxWidth: 345, marginTop: '50px' }} variant={'outlined'}>
+        //             <CardMedia
+        //                 component="img"
+        //                 height="100%"
+        //                 image={productos.img}
+        //                 alt={productos.title}
+        //             />
+        //             <CardContent>
+        //                 <Typography gutterBottom variant="h5" component="div">
+        //                     {productos.title}
+        //                 </Typography>
+        //                 <Typography variant="body2" color="text.secondary">
+        //                     {productos.itemDesc}
+        //                 </Typography>
+        //                 <Typography variant="h6" color="text.primary">
+        //                     ${productos.price}
+        //                 </Typography>
+        //             </CardContent>
+        //             <CardActions>
+        //                 <div style={{ width : '100%', display : 'flex ', flexDirection : 'column'}}>
+        //                     <Button variant="contained">VER MÁS</Button>
+        //                     <Button variant="outlined">COMPRAR</Button>
+        //                 </div>
+        //             </CardActions>
+        //         </Card>
+        //         </Grid>
+        //     )}
+        // </Grid>
     )
 };
 
